7645ea62ec5714e26227aa62e56c99199ad09234
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-07-21 22:32:40 +0100

    style: added some global style to project

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
 import App from './app/app';
 import { useTheme } from './utils/theme';
+import './styles.css';
 
 const AppContainer: React.FC<PropsWithChildren> = ({ children }) => {
   return (
