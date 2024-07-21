b9dc1de83511ac8aa3bb5978b5d45016df0f041c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-07-21 22:26:56 +0100

    conf: generated publishable toast lib

---
+++ libs/toastr/src/lib/toast.tsx
@@ -0,0 +1,14 @@
+import './toast.css';
+
+/* eslint-disable-next-line */
+export interface ToastProps {}
+
+export function Toast(props: ToastProps) {
+  return (
+    <div>
+      <h1>Welcome to Toast!</h1>
+    </div>
+  );
+}
+
+export default Toast;
